f60e93409e73ae5bbf27a192822ed8eb0a9fa6af
roblebob <robert.koppisch@gmail.com>
2024-09-29 19:52:56 +0200

    further on the path

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # experiments_in_js__befunge_interpreter
 
+[codewars](https://www.codewars.com/kata/526c7b931666d07889000a3c/train/javascript/66c0a7a5b8fe04e3b903ad68)
+
 ## Setup
 
 ```bash
---
+++ src/interpret.js
@@ -0,0 +1,196 @@
+class Plane {
+  constructor(code) {
+    this.plane = code.split("\n").map((line) => line.split(""));
+    this.pos = [0, 0]; // [row, col]
+    this.dir = [0, 1]; // [row, col]
+  }
+
+  get isFinished() {
+    return this.currentChar === "@";
+  }
+
+  get currentChar() {
+    return this.plane[this.pos[0]][this.pos[1]];
+  }
+
+  move() {
+    this.pos = [this.pos[0] + this.dir[0], this.pos[1] + this.dir[1]];
+    this.correctPos();
+  }
+
+  get isCorrectPos() {
+    return (
+      this.pos[0] >= 0 &&
+      this.pos[1] >= 0 &&
+      this.pos[0] < this.plane.length &&
+      this.pos[1] < this.plane[this.pos[0]].length
+    );
+  }
+
+  correctPos() {
+    while (!this.isCorrectPos) {
+      if (this.pos[0] === -1) this.pos[0] = this.plane.length - 1;
+      else if (this.pos[0] === this.plane.length) this.pos[0] = 0;
+      else if (this.pos[1] === -1) {
+        this.pos[1] = this.plane[this.pos[0]].length - 1;
+        this.pos[0]--;
+      } else if (this.pos[1] === this.plane[this.pos[0]].length) {
+        this.pos[1] = 0;
+        this.pos[0]++;
+      } else {
+        throw new Error(`Invalid position ${this.pos}`);
+      }
+    }
+  }
+
+  print() {
+    console.log(this.plane.map((line) => line.join("")).join("\n"));
+  }
+}
+
+function interpret(code) {
+  let _i = 0;
+  const plane = new Plane(code);
+  let x = 0;
+  let y = 0;
+
+  plane.print();
+
+  let output = "";
+  const stack = [];
+
+  const printState = () => {
+    console.log(_i++, ":");
+    if (plane.currentChar === " ") return;
+    console.log("  Pos:", plane.pos, "  Dir:", plane.dir);
+    console.log("  Char:", plane.currentChar);
+    console.log("  Stack:", JSON.stringify(stack));
+    console.log("  Output:", output);
+  };
+
+  while (!plane.isFinished && _i < 200) {
+    const char = plane.currentChar;
+    printState();
+
+    if (/\d/.test(char)) {
+      console.log("                   Pushing", +char);
+      stack.push(+char);
+    } else {
+      switch (char) {
+        case "+":
+          stack.push(stack.pop() + stack.pop());
+          break;
+        case "-":
+          stack.push(-1 * stack.pop() + stack.pop());
+          break;
+        case "*":
+          stack.push(stack.pop() * stack.pop());
+          break;
+        case "/": {
+          const a = stack.pop();
+          const b = stack.pop();
+          stack.push(a === 0 ? 0 : Math.floor(b / a));
+          break;
+        }
+        case "%": {
+          const a = stack.pop();
+          const b = stack.pop();
+          stack.push(a === 0 ? 0 : b % a);
+          break;
+        }
+        case "!":
+          stack.push(stack.pop() === 0 ? 1 : 0);
+          break;
+        case "`":
+          stack.push(stack.pop() < stack.pop() ? 1 : 0);
+          break;
+
+        case ">":
+          plane.dir = [0, 1];
+          break;
+        case "<":
+          plane.dir = [0, -1];
+          break;
+        case "^":
+          plane.dir = [-1, 0];
+          break;
+        case "v":
+          plane.dir = [1, 0];
+          break;
+        case "?":
+          plane.dir = [
+            [0, 1],
+            [0, -1],
+            [1, 0],
+            [-1, 0],
+          ][Math.floor(Math.random() * 4)];
+          break;
+
+        case "_":
+          plane.dir = stack.pop() === 0 ? [0, 1] : [0, -1];
+          break;
+        case "|":
+          plane.dir = stack.pop() === 0 ? [1, 0] : [-1, 0];
+          break;
+
+        case '"':
+          plane.move();
+          while (plane.currentChar !== '"') {
+            stack.push(plane.currentChar.charCodeAt(0));
+            plane.move();
+          }
+          break;
+
+        case ":":
+          stack.push(stack.length === 0 ? 0 : stack[stack.length - 1]);
+          break;
+
+        case "\\":
+          if (stack.length < 2) {
+            stack.push(0);
+          } else {
+            const a = stack.pop();
+            const b = stack.pop();
+            stack.push(a);
+            stack.push(b);
+          }
+          break;
+
+        case "$":
+          stack.pop();
+          break;
+
+        case ".":
+          output += stack.pop();
+          break;
+
+        case ",":
+          output += String.fromCharCode(stack.pop());
+          break;
+
+        case "#":
+          plane.move();
+          break;
+
+        case "p":
+          y = stack.pop();
+          x = stack.pop();
+          plane.plane[y][x] = String.fromCharCode(stack.pop());
+          break;
+
+        case "g":
+          y = stack.pop();
+          x = stack.pop();
+          stack.push(plane.plane[y][x]);
+          break;
+
+        case " ":
+          break;
+      }
+    }
+    plane.move();
+  }
+  return output;
+}
+
+console.log(interpret("08>:1-:v v *_$.@\n  ^    _$>\\:^"));
